89015750cc3588be7e6e27471f2e588c4ab1fe3b
dasMichal <michalxd@live.de>
2021-03-20 12:42:58 +0100

    Cleanup Imports

--- app/src/main/java/com/example/cmistodoapp/FolderSelectActivity.java
+++ app/src/main/java/com/example/cmistodoapp/FolderSelectActivity.java
@@ -1,10 +1,6 @@
 package com.example.cmistodoapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.cardview.widget.CardView;
-import androidx.drawerlayout.widget.DrawerLayout;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -12,7 +8,11 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-import com.google.android.material.card.MaterialCardView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
