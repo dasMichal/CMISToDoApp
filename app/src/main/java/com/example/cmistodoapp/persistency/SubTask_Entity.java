57e5f22d98a9e329619844fc86e3df5cde7a66f8
dasMichal <michalxd@live.de>
2021-04-28 19:51:47 +0200

    Added SubTask Room Entity

---
+++ app/src/main/java/com/example/cmistodoapp/persistency/SubTask_Entity.java
@@ -0,0 +1,87 @@
+package com.example.cmistodoapp.persistency;
+
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+import org.jetbrains.annotations.NotNull;
+
+@Entity(tableName = "SubTask_table",
+		foreignKeys = {@ForeignKey(entity = ToDo_Entity.class,
+				parentColumns = "ToDo_ID",
+				childColumns = "FK_ToDoID",
+				onDelete = ForeignKey.CASCADE)
+		})
+
+public class SubTask_Entity
+{
+
+
+	public SubTask_Entity()
+	{
+		//this.ToDoID = 0;
+		//this.SubTaskText = "name";
+		//this.isDone = false;
+
+	}
+
+
+	@NotNull
+	@ColumnInfo(name= "SubTask_Text")
+	private String SubTaskText;
+
+	@PrimaryKey(autoGenerate = true)
+	@ColumnInfo(name= "SubTask_ID")
+	private int SubTaskID;
+
+
+	@ColumnInfo(name= "SubTask_IsDone")
+	private boolean isDone;
+
+	@ColumnInfo(name= "FK_ToDoID")
+	private int FKToDoID;
+
+
+	@NotNull
+	public String getSubTaskText()
+	{
+		return SubTaskText;
+	}
+
+	public void setSubTaskText(@NotNull String subTaskText)
+	{
+		SubTaskText = subTaskText;
+	}
+
+	public int getSubTaskID()
+	{
+		return SubTaskID;
+	}
+
+	public void setSubTaskID(int subTaskID)
+	{
+		SubTaskID = subTaskID;
+	}
+
+	public boolean isDone()
+	{
+		return isDone;
+	}
+
+	public void setDone(boolean done)
+	{
+		isDone = done;
+	}
+
+	public int getFKToDoID()
+	{
+		return FKToDoID;
+	}
+
+	public void setFKToDoID(int FKToDoID)
+	{
+		this.FKToDoID = FKToDoID;
+	}
+}
