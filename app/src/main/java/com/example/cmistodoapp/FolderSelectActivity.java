4fa050de33a260c92df23bea4797b133b91abb37
dasMichal <michalxd@live.de>
2021-04-29 16:09:32 +0200

    Removed unused Placeholder Layout and referenced in Code

--- app/src/main/java/com/example/cmistodoapp/FolderSelectActivity.java
+++ app/src/main/java/com/example/cmistodoapp/FolderSelectActivity.java
@@ -5,19 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
-import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
 import android.widget.TableRow;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.cardview.widget.CardView;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -307,83 +303,6 @@ public class FolderSelectActivity extends AppCompatActivity
 
 
 	//------------------Graveyard orbit-----------------------
-	private void createGameField(List<Integer> ranNumbers, int numberFields, double average)
-	{
 
-
-		TableLayout TableGameField = findViewById(R.id.FolderTable);
-
-		TableGameField.removeAllViews();
-		TableGameField.setPadding(0,10,0,10);
-
-		TableLayout.LayoutParams TableLayout = new TableLayout.LayoutParams(android.widget.TableLayout.LayoutParams.MATCH_PARENT, android.widget.TableLayout.LayoutParams.MATCH_PARENT);
-		TableLayout.weight = 1;
-
-		TableRow.LayoutParams RowLayout = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT);
-		RowLayout.weight = 1;
-		RowLayout.gravity= Gravity.CENTER;
-		RowLayout.setMargins(10,20 ,10,20);
-
-
-
-
-		List<String> namesofFolders = new ArrayList<>();
-
-		namesofFolders.add("School");
-		namesofFolders.add("Exercise");
-		namesofFolders.add("Work");
-		namesofFolders.add("Vacation");
-
-
-
-
-		for (int i = 0; i < numberFields; i++)
-		{
-
-			//System.out.println(i%2);
-			if (i % 2 == 0)
-			{
-				//System.out.println("modulo if");
-				tr = new TableRow(this);
-				tr.setLayoutParams(TableLayout);
-				TableGameField.addView(tr);
-
-			}
-
-
-			CardView folderCard = new CardView(this);
-
-			folderCard.setMinimumWidth((int) dptopx(100));
-			folderCard.setMinimumHeight((int) dptopx(100));
-
-			TextView playCardText = new TextView(this);
-			//playCardText.setText(" "+ranNumbers.get(i));
-			playCardText.setText(" "+namesofFolders.get(i));
-			playCardText.setGravity(Gravity.CENTER);
-			playCardText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-
-
-			folderCard.setContentPadding((int) dptopx(20),(int) dptopx(20),(int) dptopx(20),(int) dptopx(20));
-			folderCard.setId(i);
-			folderCard.setTag(ranNumbers.get(i));
-			folderCard.setElevation(10);
-			folderCard.addView(playCardText);
-			folderCard.setLayoutParams(RowLayout);
-			folderCard.setOnClickListener(v -> changeActivity(folderCard.getId(),playCardText.getText().toString()));
-			tr.addView(folderCard);
-
-
-		}
-	}
-
-	public void changeActivity(int id, String s)
-	{
-		Intent intent = new Intent(FolderSelectActivity.this, ToDoList.class);
-
-		intent.putExtra("selectedFolder", s);
-		startActivity(intent);      //Start New activity
-
-
-	}
 
 }
